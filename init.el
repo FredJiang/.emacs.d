b71965e85ea80fe88bebc3f47f4a7cd6487264e8
FredJiang <270130108@qq.com>
2017-04-09 15:31:58 +0800

    no message

--- init.el
+++ init.el
@@ -81,8 +81,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'multiple-cursors)
-(global-set-key (kbd "C-n") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-p") 'mc/mark-previous-like-this)
+(global-set-key (kbd "M-n") 'mc/mark-next-like-this)
+(global-set-key (kbd "M-p") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c m") 'mc/edit-lines)
 (global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
 
