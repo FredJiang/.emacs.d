4d0955b3015467f9ed9347f96671408f6aaced9d
FredJiang <270130108@qq.com>
2017-04-24 00:17:38 +0800

    no message

--- init.el
+++ init.el
@@ -313,13 +313,13 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; https://www.emacswiki.org/emacs/AsmMode
-(defun my-asm-mode-hook ()
-  ;; you can use `comment-dwim' (M-;) for this kind of behaviour anyway
-  (local-unset-key (vector asm-comment-char))
-  ;; asm-mode sets it locally to nil, to "stay closer to the old TAB behaviour".
-  (setq tab-always-indent (default-value 'tab-always-indent)))
+; ; https://www.emacswiki.org/emacs/AsmMode
+; (defun my-asm-mode-hook ()
+;   ;; you can use `comment-dwim' (M-;) for this kind of behaviour anyway
+;   (local-unset-key (vector asm-comment-char))
+;   ;; asm-mode sets it locally to nil, to "stay closer to the old TAB behaviour".
+;   (setq tab-always-indent (default-value 'tab-always-indent)))
 
-(add-hook 'asm-mode-hook #'my-asm-mode-hook)
+; (add-hook 'asm-mode-hook #'my-asm-mode-hook)
 
 
