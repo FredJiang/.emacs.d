181bf9ed0a170a403080a76481ee7ca4d4865c25
FredJiang <270130108@qq.com>
2017-11-08 16:15:29 +0800

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
   "extends": "standard",
   "globals": {
     "gLogger": true,
-    "gProjectDir": true
+    "gProjectDir": true,
+    "gProjectDirOrder": true,
+    "gProjectDirActivity": true
   },
   "rules": {
     "space-before-function-paren": ["error", {
@@ -10,10 +12,9 @@ module.exports = {
       "named": "never",
       "asyncArrow": "always"
     }],
-    // "camelcase": 0,
+    // "camelcase": 0, // disable the rule
+    // "semi": 0 // disable the rule
     // "semi": [2, "never"]
     "semi": [2, "always"]
-    // disable the rule
-    // "semi": 0
   }
 };
