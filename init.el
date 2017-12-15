7058a861022fffa493c073f966068c4d44695504
FredJiang <270130108@qq.com>
2017-12-15 12:53:36 +0800

    no message

--- init.el
+++ init.el
@@ -663,10 +663,12 @@ there's a region, all lines that region covers will be duplicated."
 ; (list-matching-lines-default-context-lines 2)
 ; auto produced
 
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(list-matching-lines-default-context-lines 2))
+ '(list-matching-lines-default-context-lines 2)
+ '(case-fold-search nil)
+ '(case-replace nil))
+
