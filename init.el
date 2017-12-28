3c000b58bbec025908d039130aaa65e7b9943c42
FredJiang <270130108@qq.com>
2017-12-28 23:08:46 +0800

    no message

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; <autoinstall> list the packages you want
 ; auto-package-update  error: Package `emacs-24.4' is unavailable
-(setq package-list '(undo-tree window-numbering projectile multiple-cursors company auto-complete exec-path-from-shell auto-package-update windresize smooth-scrolling web-beautify highlight-parentheses js2-mode ido-completing-read+ smex go-mode go-eldoc go-autocomplete go-errcheck godoctor flycheck exec-path-from-shell magit web-mode irony company-irony company-irony-c-headers flycheck-irony yasnippet helm dumb-jump nlinum wttrin emmet-mode meghanada use-package hydra))
+(setq package-list '(undo-tree window-numbering projectile multiple-cursors company auto-complete exec-path-from-shell auto-package-update windresize smooth-scrolling web-beautify highlight-parentheses js2-mode ido-completing-read+ smex go-mode go-eldoc go-autocomplete go-errcheck godoctor flycheck exec-path-from-shell magit web-mode irony company-irony company-irony-c-headers flycheck-irony yasnippet helm dumb-jump nlinum wttrin emmet-mode meghanada use-package hydra smartparens rainbow-delimiters highlight-symbol))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
@@ -698,98 +698,82 @@ there's a region, all lines that region covers will be duplicated."
 (global-set-key (kbd "C-c g") 'dumb-jump-go)
 
 
+(require 'smartparens-config)
+(add-hook 'prog-mode-hook #'smartparens-mode)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(require 'highlight-symbol)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'meghanada)                                                           ;
-(add-hook 'java-mode-hook                                                      ;
-          (lambda ()                                                           ;
-            ;; meghanada-mode on                                               ;
-            (meghanada-mode t)                                                 ;
-            (setq c-basic-offset 2)                                            ;
-            (global-set-key (kbd "C-c b") 'meghanada-code-beautify)))          ;
-                                                                               ;
-(use-package autodisass-java-bytecode                                          ;
-  :ensure t                                                                    ;
-  :defer t)                                                                    ;
-                                                                               ;
-(add-to-list 'load-path "~/.emacs.d/mypackages/google-styleguide")             ;
-(require 'google-c-style)                                                      ;
-; (use-package google-c-style                                                  ;
-;   :defer t                                                                   ;
-;   :ensure t                                                                  ;
-;   :commands                                                                  ;
-;   (google-set-c-style))                                                      ;
-                                                                               ;
-(use-package meghanada                                                         ;
-  :defer t                                                                     ;
-  :init                                                                        ;
-  (add-hook 'java-mode-hook                                                    ;
-            (lambda ()                                                         ;
-              (google-set-c-style)                                             ;
-              (google-make-newline-indent)                                     ;
-              (meghanada-mode t)                                               ;
-              (smartparens-mode t)                                             ;
-              (rainbow-delimiters-mode t)                                      ;
-              (highlight-symbol-mode t)                                        ;
-              (global-set-key (kbd "C-c b") 'meghanada-code-beautify)))        ;
-                                                                               ;
-  :config                                                                      ;
-  (use-package realgud                                                         ;
-    :ensure t)                                                                 ;
-  (setq indent-tabs-mode nil)                                                  ;
-  (setq tab-width 2)                                                           ;
-  (setq c-basic-offset 2)                                                      ;
-  (setq meghanada-server-remote-debug t)                                       ;
-  (setq meghanada-javac-xlint "-Xlint:all,-processing")                        ;
-  :bind                                                                        ;
-  (:map meghanada-mode-map                                                     ;
-        ("C-S-t" . meghanada-switch-testcase)                                  ;
-        ("M-RET" . meghanada-local-variable)                                   ;
-        ("C-M-." . helm-imenu)                                                 ;
-        ("M-r" . meghanada-reference)                                          ;
-        ("M-t" . meghanada-typeinfo)                                           ;
-        ("C-z" . hydra-meghanada/body))                                        ;
-  :commands                                                                    ;
-  (meghanada-mode))                                                            ;
-                                                                               ;
-(defhydra hydra-meghanada (:hint nil :exit t)                                  ;
-"                                                                              ;
-^Edit^                           ^Tast or Task^                                ;
-^^^^^^-------------------------------------------------------                  ;
-_f_: meghanada-compile-file      _m_: meghanada-restart                        ;
-_c_: meghanada-compile-project   _t_: meghanada-run-task                       ;
-_o_: meghanada-optimize-import   _j_: meghanada-run-junit-test-case            ;
-_s_: meghanada-switch-test-case  _J_: meghanada-run-junit-class                ;
-_v_: meghanada-local-variable    _R_: meghanada-run-junit-recent               ;
-_i_: meghanada-import-all        _r_: meghanada-reference                      ;
-_g_: magit-status                _T_: meghanada-typeinfo                       ;
-_l_: helm-ls-git-ls                                                            ;
-_q_: exit                                                                      ;
-"                                                                              ;
-  ("f" meghanada-compile-file)                                                 ;
-  ("m" meghanada-restart)                                                      ;
-                                                                               ;
-  ("c" meghanada-compile-project)                                              ;
-  ("o" meghanada-optimize-import)                                              ;
-  ("s" meghanada-switch-test-case)                                             ;
-  ("v" meghanada-local-variable)                                               ;
-  ("i" meghanada-import-all)                                                   ;
-                                                                               ;
-  ("g" magit-status)                                                           ;
-  ("l" helm-ls-git-ls)                                                         ;
-                                                                               ;
-  ("t" meghanada-run-task)                                                     ;
-  ("T" meghanada-typeinfo)                                                     ;
-  ("j" meghanada-run-junit-test-case)                                          ;
-  ("J" meghanada-run-junit-class)                                              ;
-  ("R" meghanada-run-junit-recent)                                             ;
-  ("r" meghanada-reference)                                                    ;
-                                                                               ;
-  ("q" exit)                                                                   ;
-  ("z" nil "leave"))                                                           ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; ↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧
+(use-package autodisass-java-bytecode
+  :ensure t
+  :defer t)
 
+(use-package meghanada
+  :defer t
+  :init
+  (add-hook 'java-mode-hook
+            (lambda ()
+              (meghanada-mode t)
+              (global-set-key (kbd "C-c b") 'meghanada-code-beautify)))
+
+  :config
+  (use-package realgud
+    :ensure t)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2)
+  (setq c-basic-offset 2)
+  (setq meghanada-server-remote-debug t)
+  (setq meghanada-javac-xlint "-Xlint:all,-processing")
+  :bind
+  (:map meghanada-mode-map
+        ("C-S-t" . meghanada-switch-testcase)
+        ("M-RET" . meghanada-local-variable)
+        ("C-M-." . helm-imenu)
+        ("M-r" . meghanada-reference)
+        ("M-t" . meghanada-typeinfo)
+        ("C-z" . hydra-meghanada/body))
+  :commands
+  (meghanada-mode))
+
+(defhydra hydra-meghanada (:hint nil :exit t)
+"
+^Edit^                           ^Tast or Task^
+^^^^^^-------------------------------------------------------
+_b_: meghanada-code-beautify
+_f_: meghanada-compile-file      _m_: meghanada-restart
+_c_: meghanada-compile-project   _t_: meghanada-run-task
+_o_: meghanada-optimize-import   _j_: meghanada-run-junit-test-case
+_s_: meghanada-switch-test-case  _J_: meghanada-run-junit-class
+_v_: meghanada-local-variable    _R_: meghanada-run-junit-recent
+_i_: meghanada-import-all        _r_: meghanada-reference
+_g_: magit-status                _T_: meghanada-typeinfo
+_l_: helm-ls-git-ls
+_q_: exit
+"
+  ("b" meghanada-code-beautify)
+  ("f" meghanada-compile-file)
+  ("m" meghanada-restart)
+
+  ("c" meghanada-compile-project)
+  ("o" meghanada-optimize-import)
+  ("s" meghanada-switch-test-case)
+  ("v" meghanada-local-variable)
+  ("i" meghanada-import-all)
+
+  ("g" magit-status)
+  ("l" helm-ls-git-ls)
+
+  ("t" meghanada-run-task)
+  ("T" meghanada-typeinfo)
+  ("j" meghanada-run-junit-test-case)
+  ("J" meghanada-run-junit-class)
+  ("R" meghanada-run-junit-recent)
+  ("r" meghanada-reference)
+
+  ("q" exit)
+  ("z" nil "leave"))
+; ↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥
 
 
 
