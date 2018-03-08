7b22829738e50dd74014143f8b2b22a0a8ced7c0
FredJiang <270130108@qq.com>
2018-03-08 16:06:53 +0800

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,41 @@
 module.exports = {
-  "extends": "standard",
-  "globals": {
-    "gLogger": true,
-    "gConfigs": true,
-    "gConfigsAct": true,
-    "gConfigsOrder": true,
-    "gProjectDir": true,
-    "gProjectDirAct": true,
-    "gProjectDirOrder": true
+  'extends' : 'standard',
+  'globals' : {
+    'gLogger'          : true,
+    'gConfigs'         : true,
+    'gConfigsAct'      : true,
+    'gConfigsOrder'    : true,
+    'gProjectDir'      : true,
+    'gProjectDirAct'   : true,
+    'gProjectDirOrder' : true
   },
-  "rules": {
-    "space-before-function-paren": ["error", {
-      "anonymous": "always",
-      "named": "never",
-      "asyncArrow": "always"
+  'rules': {
+    'space-before-function-paren': ['error', {
+      'anonymous'  : 'always',
+      'named'      : 'never',
+      'asyncArrow' : 'always'
     }],
-    "key-spacing": ["error", {
-      "multiLine": {
-        "beforeColon": false,
-        "afterColon": true
+    'key-spacing': ['error', {
+      'multiLine': {
+        'beforeColon' : false,
+        'afterColon'  : true
       },
-      "align": {
-        "beforeColon": true,
-        "afterColon": true,
-        "on": "colon"
+      'align': {
+        'beforeColon' : true,
+        'afterColon'  : true,
+        'on'          : 'colon'
+      }
+    }],
+    'no-multi-spaces': ['error', {
+      'exceptions': {
+        'VariableDeclarator'   : true,
+        'ImportDeclaration'    : true,
+        'AssignmentExpression' : true
       }
     }],
     // "camelcase": 0, // disable the rule
     // "semi": 0 // disable the rule
     // "semi": [2, "never"]
-    "semi": [2, "always"]
+    'semi': [2, 'always']
   }
 };
